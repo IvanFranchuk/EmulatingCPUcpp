62ed43b97070bcc32c8f8f48ca6a096290ec826d
Ivan Franchuk <franchukivan123@gmail.com>
2023-09-06 18:29:59 +0300

    class cpu

--- main_6502/main_6502.cpp
+++ main_6502/main_6502.cpp
@@ -3,6 +3,27 @@
 #include <stdlib.h>
 
 
+
+struct CPU
+{
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC;    //program counter
+    Word SP;    //stack pointer
+
+    Byte A, X, Y;   //registers
+
+    // processor status
+    Byte C : 1; // carry flag
+    Byte Z : 1; // zero flag
+    Byte I : 1; // interrupt disable
+    Byte D : 1; // decimal mode
+    Byte B : 1; // break command
+    Byte V : 1; // overflow flag
+    Byte N : 1; // negative flag
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
